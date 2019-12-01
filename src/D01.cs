adcaf7f128b9b69996c97c02041713fb57358f24
Oaz <github@azeau.com>
2019-12-01 12:17:06 +0100

    D01 part 2

--- src/D01.cs
+++ src/D01.cs
@@ -3,10 +3,13 @@ namespace src
   using System;
   using System.Collections.Generic;
   using System.Linq;
+  using System.Reactive.Linq;
 
   public static class D01
   {
     public static int FuelForMass(int mass) => mass/3 - 2;
+    public static int TotalFuelForMass(int mass) 
+     => Observable.Generate(mass, m => m > 0, m => FuelForMass(m), m => m).ToEnumerable().Skip(1).Sum();
 
     public static int SumOf(this IEnumerable<string> input, Func<int,int> getValue)
       => input.Select(l => getValue(int.Parse(l))).Sum();
--- tests/D01.cs
+++ tests/D01.cs
@@ -31,5 +31,30 @@ namespace tests
                 File.ReadAllLines("D01.txt").SumOf(D01.FuelForMass)
             ).IsEqualTo(3401852);
         }
+
+        [TestCase(12, 2)]
+        [TestCase(14, 2)]
+        [TestCase(1969, 966)]
+        [TestCase(100756, 50346)]
+        public void GetTotalFuelForMass(int givenMass, int expectedFuel)
+        {
+            Check.That(D01.TotalFuelForMass(givenMass)).IsEqualTo(expectedFuel);
+        }
+        
+        [Test]
+        public void GetSumOfTotalFuelForMasses()
+        {
+            Check.That(
+                new string[] {"12", "14", "1969"}.SumOf(D01.TotalFuelForMass)
+            ).IsEqualTo(970);
+        }
+        
+        [Test]
+        public void Part2()
+        {
+            Check.That(
+                File.ReadAllLines("D01.txt").SumOf(D01.TotalFuelForMass)
+            ).IsEqualTo(5099916);
+        }
     }
 }
