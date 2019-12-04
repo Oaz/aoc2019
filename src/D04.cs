3ea92339930286ce52d2fd7f9f700c0ee6793ef8
Oaz <github@azeau.com>
2019-12-04 07:55:58 +0100

    Day 4 part 2

--- src/D04.cs
+++ src/D04.cs
@@ -1,6 +1,8 @@
 namespace src04
 {
+  using System;
   using System.Linq;
+  using System.Collections.Generic;
 
   public static class Code
   {
@@ -10,11 +12,35 @@ namespace src04
      => Enumerable.Range(0,s.Length-1).Select(i => s[i+1]-s[i]).All(x => x>=0);
     public static bool IsGoodPassword(string s)
      => HasTwoAdjacentIdenticalChars(s) && CharsAlwaysIncrease(s);
-    public static int CountGoodPassword(int begin, int end)
+    public static int CountPassword(int begin, int end, Func<string,bool> criteria)
      => Enumerable
           .Range(begin,end-begin+1)
           .Select(i => i.ToString())
-          .Where(s => IsGoodPassword(s))
+          .Where(s => criteria(s))
           .Count();
+
+    public static IEnumerable<int> SizesOfIdenticalGroups(string s)
+      => s.Take(s.Length-1)
+          .Zip(s.Skip(1))
+          .Select(g => g.First == g.Second)
+          .Aggregate(
+            Enumerable.Repeat(0,1),
+            (sizes,identicalChars) => identicalChars
+              ? sizes.Skip(1).Prepend(sizes.First()+1)
+              : sizes.Prepend(0)
+          ).Where(size => size > 0)
+          .Select(size => size+1)
+          .Reverse();
+
+    public static IList<T> ChangeHead<T>(this IList<T> l, Func<T,T> f)
+    {
+      l[0] = f(l[0]);
+      return l;
+    }
+
+    public static bool HasAcceptableGroupSizes(string s) => SizesOfIdenticalGroups(s).Contains(2);
+
+    public static bool IsReallyGoodPassword(string s)
+     => HasAcceptableGroupSizes(s) && CharsAlwaysIncrease(s);
   }
 }
--- tests/D04.cs
+++ tests/D04.cs
@@ -36,8 +36,33 @@ namespace tests04
         [Test]
         public void Part1()
         {
-            Check.That(Code.CountGoodPassword(231832,767346)).IsEqualTo(1330);
+            Check.That(Code.CountPassword(231832,767346,Code.IsGoodPassword)).IsEqualTo(1330);
         }
 
+        [TestCase("123456",new int[] {})]
+        [TestCase("113456",new int[] {2})]
+        [TestCase("112233",new int[] {2,2,2})]
+        [TestCase("123444",new int[] {3})]
+        [TestCase("111122",new int[] {4,2})]
+        public void SizesOfIdenticalGroups(string number, int[] expectedResult)
+        {
+            Check.That(Code.SizesOfIdenticalGroups(number)).IsEqualTo(expectedResult);
+        }
+
+        [TestCase("123456",false)]
+        [TestCase("113456",true)]
+        [TestCase("112233",true)]
+        [TestCase("123444",false)]
+        [TestCase("111122",true)]
+        public void HasAcceptableGroupSizes(string number, bool expectedResult)
+        {
+            Check.That(Code.HasAcceptableGroupSizes(number)).IsEqualTo(expectedResult);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Check.That(Code.CountPassword(231832,767346,Code.IsReallyGoodPassword)).IsEqualTo(876);
+        }
     }
 }
