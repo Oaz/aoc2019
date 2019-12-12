575e09d49f37895cef776bc5eb31f3a3ea738c38
Oaz <github@azeau.com>
2019-12-12 08:47:56 +0100

    Day 12 Part 1

---
+++ src/D12.cs
@@ -0,0 +1,95 @@
+namespace src12
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Reactive.Linq;
+  using System.Text.RegularExpressions;
+
+  public readonly struct Moon
+  {
+    public Moon(string def)
+    {
+      var match = Regex.Match(def, "<x=([0-9-]+), y=([0-9-]+), z=([0-9-]+)>");
+      var coords = match.Groups.Cast<Group>().Skip(1).Select(g => int.Parse(g.Value)).ToArray();
+      Position = Coords3.At(coords[0], coords[1], coords[2]);
+      Velocity = Coords3.At(0, 0, 0);
+    }
+    public Moon(Coords3 p, Coords3 v)
+    {
+      Position = p;
+      Velocity = v;
+    }
+    public readonly Coords3 Position;
+    public readonly Coords3 Velocity;
+    public override string ToString() => $"{Position},{Velocity}";
+    public int PotentialEnergy => Position.Length;
+    public int KineticEnergy => Velocity.Length;
+    public int TotalEnergy => PotentialEnergy * KineticEnergy;
+    public Coords3 Gravity(Moon other) => Coords3.At(
+        Position.X > other.Position.X ? -1 : (Position.X < other.Position.X ? 1 : 0),
+        Position.Y > other.Position.Y ? -1 : (Position.Y < other.Position.Y ? 1 : 0),
+        Position.Z > other.Position.Z ? -1 : (Position.Z < other.Position.Z ? 1 : 0)
+      );
+
+    public Moon ApplyGravity(Coords3 g) => new Moon(Position,Velocity+g);
+
+  }
+
+  public readonly struct Universe
+  {
+    public Universe(IEnumerable<string> defs) => Moons = defs.Select(d => new Moon(d)).ToArray();      
+    public Universe(Moon[] moons) => Moons = moons;
+
+    public readonly Moon[] Moons;
+    public Universe Step()
+    {
+      var s = Moons.ToArray();
+      for (int i = 0; i < s.Length; i++)
+      {
+        var moon1 = s[i];
+        for (int j = i + 1; j < s.Length; j++)
+        {
+          var moon2 = s[j];
+          var g = moon2.Gravity(moon1);
+          s[j] = s[j].ApplyGravity(g);
+          s[i] = s[i].ApplyGravity(-g);
+        }
+      }
+      var u = s.Select(m => new Moon(m.Position+m.Velocity,m.Velocity)).ToArray();
+      return new Universe(u);
+    }
+
+    public Universe Steps(int steps) =>
+      Enumerable.Range(0,steps).Aggregate(this, (u,i) => u.Step());
+
+    public int TotalEnergy => Moons.Select(m => m.TotalEnergy).Sum();
+  }
+  public readonly struct Coords3
+  {
+    public static Coords3 At(int x, int y, int z) => new Coords3(x, y, z);
+    public Coords3(int x, int y, int z) { X = x; Y = y; Z = z; }
+    public readonly int X;
+    public readonly int Y;
+    public readonly int Z;
+
+    public Coords3 Abs => Coords3.At(Math.Abs(X), Math.Abs(Y), Math.Abs(Z));
+    public int Length => Abs.X + Abs.Y + Abs.Z;
+
+    public override string ToString() => $"({X},{Y},{Z})";
+
+    public static Coords3 operator +(Coords3 a, Coords3 b) => At(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+    public static Coords3 operator -(Coords3 a, Coords3 b) => At(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+    public static Coords3 operator -(Coords3 a) => At(-a.X, -a.Y, -a.Z);
+  }
+
+  public static class CoordsExtensions
+  {
+    public static Coords3 Bulk(this IEnumerable<Coords3> l, Func<IEnumerable<int>, int> f) =>
+      Coords3.At(f(l.Select(c => c.X)), f(l.Select(c => c.Y)), f(l.Select(c => c.Z)));
+    public static Coords3 Min(this IEnumerable<Coords3> l) => l.Bulk(x => x.Min());
+    public static Coords3 Max(this IEnumerable<Coords3> l) => l.Bulk(x => x.Max());
+
+
+  }
+}
---
+++ tests/D12.cs
@@ -0,0 +1,59 @@
+namespace tests12
+{
+  using NUnit.Framework;
+  using NFluent;
+  using System.IO;
+  using System.Numerics;
+  using System.Linq;
+  using src12;
+
+  public class Tests
+  {
+
+    [TestCase(0, "(-1,0,2),(0,0,0)", "(2,-10,-7),(0,0,0)", "(4,-8,8),(0,0,0)", "(3,5,-1),(0,0,0)")]
+    [TestCase(1, "(2,-1,1),(3,-1,-1)", "(3,-7,-4),(1,3,3)", "(1,-7,5),(-3,1,-3)", "(2,2,0),(-1,-3,1)")]
+    public void Steps(int steps, string moon0, string moon1, string moon2, string moon3)
+    {
+      var newMoons = new Universe(example1).Steps(steps).Moons;
+      Check.That(newMoons[0].ToString()).IsEqualTo(moon0);
+      Check.That(newMoons[1].ToString()).IsEqualTo(moon1);
+      Check.That(newMoons[2].ToString()).IsEqualTo(moon2);
+      Check.That(newMoons[3].ToString()).IsEqualTo(moon3);
+    }
+
+    [TestCase(0, 6, 6, 36)]
+    [TestCase(1, 9, 5, 45)]
+    [TestCase(2, 10, 8, 80)]
+    [TestCase(3, 6, 3, 18)]
+    public void Energy(int i, int pot, int kin, int tot)
+    {
+      var newMoons = new Universe(example1).Steps(10).Moons;
+      Check.That(newMoons[i].PotentialEnergy).IsEqualTo(pot);
+      Check.That(newMoons[i].KineticEnergy).IsEqualTo(kin);
+      Check.That(newMoons[i].TotalEnergy).IsEqualTo(tot);
+    }
+
+    [Test]
+    public void TotalEnergy() =>
+      Check.That(new Universe(example1).Steps(10).TotalEnergy).IsEqualTo(179);
+
+    [Test]
+    public void Part1() =>
+      Check.That(new Universe(MySystem).Steps(1000).TotalEnergy).IsEqualTo(7928);
+
+    public string[] MySystem => File.ReadAllLines("D12.txt");
+
+    readonly string[] example1 = new string[] {
+      "<x=-1, y=0, z=2>",
+      "<x=2, y=-10, z=-7>",
+      "<x=4, y=-8, z=8>",
+      "<x=3, y=5, z=-1>"
+      };
+    readonly string[] example2 = new string[] {
+      "<x=-8, y=-10, z=0>",
+      "<x=5, y=5, z=10>",
+      "<x=2, y=-7, z=3>",
+      "<x=9, y=-8, z=-3>"
+      };
+  }
+}
